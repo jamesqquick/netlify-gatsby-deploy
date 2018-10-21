0770340df0e3a9963623bc7e2512174a42d43193
James Quick <james.q.quick@outlook.com>
2018-10-21 14:15:13 -0500

    Updated index page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Netlify Gatsby Deploy!</h1>
+    <h1>Netlify Gatsby Deploy Updated!!!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
